3710245fc3be8650ce03a98b71d22039fcd554e8
zota <zota@example.com>
2024-11-04 07:44:50 +0900

    block draw

--- main.cpp
+++ main.cpp
@@ -20,28 +20,90 @@ public:
 private:
     float fBatPos = 20.0f;
     float fBatWidth = 40.0f;
-    olc::vf2d vBall = { 200.0f, 200.0f };
-    olc::vf2d vBallVel = { 200.0f, -100.0f };
+    float fBatSpeed = 250.0f;
 
     olc::vf2d vBallPos = { 0.0f, 0.0f };
     olc::vf2d vBallDir = { 0.0f, 0.0f };
-    float fBatSpeed = 250.0f;
+    float fBatSpeed = 0.0f;
     float fBallRadius = 5.0f;
+
+    olc::vi2d vBlockSize = {16,16};
+    std::unique_ptr<int[]> blocks;
+
+    std::unique_ptr<olc::Sprite> sprTile;
+
     
 public:
 	bool OnUserCreate() override
 	{
-        srand(100);
+        blocks = std::make_unique<int[]>(24 * 30);
+        for (int y = 0; y < 30; y++)
+        {
+            for (int x = 0; x < 24; x++)
+            {
+                if (x == 0 || y == 0 || x == 23)
+                    blocks[y * 24 + x] = 10;
+                else
+                    blocks[y * 24 + x] = 0;
+
+                if (x > 2 && x <= 20 && y > 3 && y <= 5)
+                    blocks[y * 24 + x] = 1;
+                if (x > 2 && x <= 20 && y > 3 && y <= 5)
+                    blocks[y * 24 + x] = 2;
+                if (x > 2 && x <= 20 && y > 3 && y <= 5)
+                    blocks[y * 24 + x] = 3;
+            }
+        }
+
+        // Load the sprite
+        sprTile = std::make_unique<olc::Sprite>("./gfx/tut_tiles.png");
+
+        // Start Ball
+        float fAngle = float(rand()) /float(RAND_MAX) * 2.0f * 3.14159;
+        fAngle = -0.4;
+        vBallDir = { cos(fAngle), sin(fAngle) };
+        vBallPos = { 12.5f, 12.5f };
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-        // Start Ball
-        float fAngle = float(rand()) / float(RAND_MAX) * 2.0f * 3.14159f;
-        fAngle = -0.4f;
-        vBallDir = { cos(fAngle), sin(fAngle) };
-        fBallPos = { 12.5f, 15.5f };
+        // Calculate where ball should be, if no collision
+        olc::vf2d vPotentialBallPos = vBallPos + vBallDir * fBallSpeed * fElapsedTime;
+
+        // Test for hits 4 points around ball
+        olc::vf2d vTileBallRadialDims = {fBallRadius / vBlockSize.x, fBallRadius / vBalockSize.y};
+
+        auto TestResolveCollisionPoint = [&](const olc::vf2d& point)
+        {
+            olc::vi2d vTestPoint = vPotentialBallPos + vTileBallRadialDims * point;
+            auto& tile = blocks[vTestPoint.y * 24 + vTestPoint.x];
+            if(tile == 0)
+            {
+                // Do Nothing, no collision
+                return false;
+            }
+            else
+            {
+                // Ball has collided with a tile
+                bool bTileHit = tile < 10;
+                if(bTileHit) tile--;
+
+                // Collision response
+                if(point.x == 0.0f)
+                    vBallDir.y *= -1.0f;
+                if(point.y == 0.0f)
+                    vBallDir.x *= -1.0f;
+                return bTileHit;
+            }
+        };
+
+        // four sides
+        bool bHasHitTile = false;
+        bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(0, -1));
+        bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(0, +1));
+        bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(-1, 0));
+        bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(+1, 0));
 
         // Handle User Input
         if (GetKey(olc::Key::LEFT).bHeld) fBatPos -= fBatSpeed * fElapsedTime;
@@ -52,18 +114,42 @@ public:
         // Update Ball
         vBall += vBallVel * fElapsedTime;
 
+        // Fake Floor
+        if(vBallPos.y > 20.0f) vBallDir.y *= -1.0f;
 
-        // Calculate where ball should be, if no collision
-        olc::vf2d vPotentialBallPos = vBallPos + vBallDir * fBallSpeed * fElapsedTime;
-        // Test for hits 4 points around ball
-        olc::vf2d vTileBallRadialDims = {fBallRadius / vBlockSize.x, fBallRadius / vBalockSize.y};
+        // Actually update ball position with modified direction
+        vBallPos += vBallDir * fBallSpeed * fElapsedTime;
 
-        // four sides
-        bool bHasHitTile = false;
-        bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(0, -1));
-        bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(0, +1));
-        bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(-1, 0));
-        bHasHitTile |= TestResolveCollisionPoint(olc::vf2d(+1, 0));
+        // Erase previous frame
+        Clear(olc::DARK_BLUE);
+        SetPixelMode(olc::Pixel::MASK);
+        for (int y = 0; y < 30; y++)
+        {
+            for (int x = 0; x < 24; x++)
+            {
+                switch (blocks[y * 24 + x])
+                {
+                    case 0:
+                        break;
+                    case 10: // Draw Boundary
+                        DrawPartialSprite(olc::vi2d(x,y) * vBlockSize, sprTile.get(), olc::vi2d(0,0) * vBlockSize, vBlockSize);
+                        break;
+                    case 1:
+                        DrawPartialSprite(olc::vi2d(x,y) * vBlockSize, sprTile.get(), olc::vi2d(1,0) * vBlockSize, vBlockSize);
+                        break;
+                    case 2:
+                        DrawPartialSprite(olc::vi2d(x,y) * vBlockSize, sprTile.get(), olc::vi2d(2,0) * vBlockSize, vBlockSize);
+                        break;
+                    case 3:
+                        DrawPartialSprite(olc::vi2d(x,y) * vBlockSize, sprTile.get(), olc::vi2d(3,0) * vBlockSize, vBlockSize);
+                        break;
+                }
+            }
+        }
+        SetPixelMode(olc::Pixel::NORMAL);
+
+        //
+        
 
         // Arena detection
         if(vBall.y <= 10.0f) vBallVel.y *= -1.0f;
@@ -83,56 +169,15 @@ public:
             vBallVel = {300.0f * cos(fAngle), 300.0f * sin(fAngle)};
         }
 
-        if(vBallPos.y > 20.0f) vBallDir.y *= -1.0f;
-
-        // Actually update ball position with modified direction
-        vBallPos += vBallDir * fBallSpeed * fElapsedTime;
-
-        // Erase previous frame
-        Clear(olc::DARK_BLUE);
-
-        // Draw Boundary
-        DrawLine(10, 10, ScreenWidth() - 10, 10, olc::YELLOW);
-        DrawLine(10, 10, 10, ScreenHeight() - 10, olc::YELLOW);
-        DrawLine(ScreenWidth() - 10, 10, ScreenWidth() - 10, ScreenHeight() - 10, olc::YELLOW);
-
-        // Draw Bat
-        FillRect(int(fBatPos), ScreenHeight() - 20, int(fBatWidth), 10, olc::GREEN);
+//        // Draw Bat
+//        FillRect(int(fBatPos), ScreenHeight() - 20, int(fBatWidth), 10, olc::GREEN);
 
         // Draw Ball
         FillCircle(vBallPos * vBlockSize, fBallRadius, olc::CYAN);
-
-        if (GetMouse(0).bHeld)
-        {
-            vBall = { float(GetMouseX()), float(GetMouseY()) };
-        }
 		return true;
 	}
 };
 
-auto TestResolveCollisionPoint = [&](const olc::vf2d& point)
-{
-    olc::vi2d vTestPoint = vPotentialBallPos + vTileBallRadialDims * point;
-    auto& tile = blocks[vTestPoint.y * 24 + vTestPoint.x];
-    if(tile == 0)
-    {
-        // Do Nothing, no collision
-        return false;
-    }
-    else
-    {
-        // Ball has collided with a tile
-        bool bTileHit = tile < 10;
-        if(bTileHit) tile--;
-
-        // Collision response
-        if(point.x == 0.0f)
-            vBallDir.y *= -1.0f;
-        if(point.y == 0.0f)
-            vBallDir.x *= -1.0f;
-        return bTileHit;
-    }
-};
 
 
 int main()
